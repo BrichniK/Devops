a34d27ad5413f5f1e4956ec082fbce7d39162eb7
echaouch <elhem.chaouch@esprit.tn>
2025-01-20 20:03:29 +0100

    validate test unitaires

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -1,10 +1,27 @@
 package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import tn.esprit.spring.repositories.*;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class GestionStationSkiApplicationTests {
+	@MockBean
+	private ICourseRepository courseRepository;
+	@MockBean
+	private IInstructorRepository instructorRepository;
+	@MockBean
+	private IPisteRepository pisteRepository;
+	@MockBean
+	private IRegistrationRepository registrationRepository;
+	@MockBean
+	private ISkierRepository skierRepository;
+	@MockBean
+	private ISubscriptionRepository subscriptionRepository;
 
 	@Test
 	void contextLoads() {
